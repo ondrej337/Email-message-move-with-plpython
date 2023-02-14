52e111f2092ec584d7dea65209b71f75e5fd5548
Ondrej Koval <40807038+ondrej337@users.noreply.github.com>
2023-02-14 14:51:28 +0100

    Add files via upload

---
+++ userbox.sql
@@ -0,0 +1,40 @@
+/*
+ Navicat PostgreSQL Data Transfer
+
+ Source Server         : doma@db_server
+ Source Server Type    : PostgreSQL
+ Source Server Version : 150002 (150002)
+ Source Host           : localhost:5432
+ Source Catalog        : doma
+ Source Schema         : mycron
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 150002 (150002)
+ File Encoding         : 65001
+
+ Date: 14/02/2023 14:46:45
+*/
+
+
+-- ----------------------------
+-- Table structure for userbox
+-- ----------------------------
+DROP TABLE IF EXISTS "mycron"."userbox";
+CREATE TABLE "mycron"."userbox" (
+  "id_mail" int4 NOT NULL,
+  "current_box" varchar(255) COLLATE "pg_catalog"."default",
+  "destination_box" varchar(255) COLLATE "pg_catalog"."default",
+  "from" varchar(255) COLLATE "pg_catalog"."default",
+  "subject" varchar(255) COLLATE "pg_catalog"."default",
+  "attachment" varchar(255) COLLATE "pg_catalog"."default",
+  "os_current_dir" varchar(255) COLLATE "pg_catalog"."default",
+  "os_destination_dir" varchar(255) COLLATE "pg_catalog"."default",
+  "group" char(2) COLLATE "pg_catalog"."default",
+  "audit_date" timestamp(6) DEFAULT now()
+)
+;
+
+-- ----------------------------
+-- Foreign Keys structure for table userbox
+-- ----------------------------
+ALTER TABLE "mycron"."userbox" ADD CONSTRAINT "idx_id_mail" FOREIGN KEY ("id_mail") REFERENCES "mycron"."usermail" ("id_mail") ON DELETE NO ACTION ON UPDATE NO ACTION;
---
+++ usermail.sql
@@ -0,0 +1,40 @@
+/*
+ Navicat PostgreSQL Data Transfer
+
+ Source Server         : doma@db_server
+ Source Server Type    : PostgreSQL
+ Source Server Version : 150002 (150002)
+ Source Host           : localhost:5432
+ Source Catalog        : doma
+ Source Schema         : mycron
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 150002 (150002)
+ File Encoding         : 65001
+
+ Date: 14/02/2023 14:47:06
+*/
+
+
+-- ----------------------------
+-- Table structure for usermail
+-- ----------------------------
+DROP TABLE IF EXISTS "mycron"."usermail";
+CREATE TABLE "mycron"."usermail" (
+  "id_mail" int4 NOT NULL DEFAULT nextval('"mycron".usermail_id_mail_seq'::regclass),
+  "hostname" varchar(255) COLLATE "pg_catalog"."default",
+  "email" text COLLATE "pg_catalog"."default" NOT NULL,
+  "password" text COLLATE "pg_catalog"."default",
+  "audit_date" timestamp(6) DEFAULT now()
+)
+;
+
+-- ----------------------------
+-- Uniques structure for table usermail
+-- ----------------------------
+ALTER TABLE "mycron"."usermail" ADD CONSTRAINT "umail" UNIQUE ("email");
+
+-- ----------------------------
+-- Primary Key structure for table usermail
+-- ----------------------------
+ALTER TABLE "mycron"."usermail" ADD CONSTRAINT "usermail_pkey" PRIMARY KEY ("id_mail");
